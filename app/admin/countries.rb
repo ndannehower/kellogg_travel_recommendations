311b8fd14b56c2054532177a76379b6031871f0a
Raghu Betina <raghu@firstdraft.com>
2018-06-14 05:17:53 +0000

    Whitelist columns in Country for ActiveAdmin

--- app/admin/countries.rb
+++ app/admin/countries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Country do
+
+ permit_params :country
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
