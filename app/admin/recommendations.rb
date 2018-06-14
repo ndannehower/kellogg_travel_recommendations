aa3d00b383fc83c84e0b6e63ed200148042f1cfc
Raghu Betina <raghu@firstdraft.com>
2018-06-14 05:17:50 +0000

    Whitelist columns in Recommendation for ActiveAdmin

--- app/admin/recommendations.rb
+++ app/admin/recommendations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Recommendation do
+
+ permit_params :recommendation, :category_id, :country_id, :city_id, :address, :good_for, :photo, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
