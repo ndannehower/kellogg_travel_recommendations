eaaa753e0f475fd91a997de0e407d5124ac34770
Raghu Betina <raghu@firstdraft.com>
2018-06-14 15:44:17 +0000

    4th draft

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,83 @@
+class TripsController < ApplicationController
+  def index
+    @q = Trip.ransack(params[:q])
+    @trips = @q.result(:distinct => true).includes(:year, :recommendations).page(params[:page]).per(10)
+
+    render("trips/index.html.erb")
+  end
+
+  def show
+    @recommendation = Recommendation.new
+    @trip = Trip.find(params[:id])
+
+    render("trips/show.html.erb")
+  end
+
+  def new
+    @trip = Trip.new
+
+    render("trips/new.html.erb")
+  end
+
+  def create
+    @trip = Trip.new
+
+    @trip.trip = params[:trip]
+    @trip.year_id = params[:year_id]
+
+    save_status = @trip.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/trips/new", "/create_trip"
+        redirect_to("/trips")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Trip created successfully.")
+      end
+    else
+      render("trips/new.html.erb")
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+
+    render("trips/edit.html.erb")
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+
+    @trip.trip = params[:trip]
+    @trip.year_id = params[:year_id]
+
+    save_status = @trip.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/trips/#{@trip.id}/edit", "/update_trip"
+        redirect_to("/trips/#{@trip.id}", :notice => "Trip updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Trip updated successfully.")
+      end
+    else
+      render("trips/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+
+    @trip.destroy
+
+    if URI(request.referer).path == "/trips/#{@trip.id}"
+      redirect_to("/", :notice => "Trip deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Trip deleted.")
+    end
+  end
+end
--- db/migrate/20180614154000_devise_create_users.rb
+++ db/migrate/20180614154000_devise_create_users.rb
@@ -34,6 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
       t.string :email
       t.string :password
+      t.string :name
 
       t.timestamps null: false
     end
